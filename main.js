5fe34d6679e9c181db854f9e490c50ee02eeadbe
Charlotte Stone <charlie.stone649@gmail.com>
2024-07-07 13:45:38 +0100

    Update stylings

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let mainWindow;
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 550,
-        height: 600,
+        height: 650,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
